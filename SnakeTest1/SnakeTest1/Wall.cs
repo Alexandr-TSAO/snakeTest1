394e8e823bdc4d0ceecde29da5cd4fa54ceb8187
Alexandr-TSAO <alexander.tsapov@it-grad.ru>
2016-03-09 00:32:34 +0300

    add

---
+++ SnakeTest1/SnakeTest1/Wall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeTest1
+{
+    class Wall
+    {
+        public List<Point> pWall;
+
+        public Wall(int xLeft, int xRight, int yTop, int yDown, char sym)
+        {
+            pWall = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                for (int y = yTop; y <= yDown; y++)
+                {
+                    Point p = new Point(x, y, sym);
+                    pWall.Add(p);
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point i in pWall)
+            { i.Draw(); }
+        }
+    }
+}
